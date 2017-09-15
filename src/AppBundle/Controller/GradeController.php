b439e4407b09d3f33982faaad2b7c70161ea1209
David Larrimore <davidlarrimore@gmail.com>
2017-09-15 01:52:01 -0400

    filter

--- src/AppBundle/Controller/GradeController.php
+++ src/AppBundle/Controller/GradeController.php
@@ -7,9 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\Grade;
-
+use AppBundle\Utils\CSVHelper;
+use AppBundle\Utils\CampaignHelper;
+use AppBundle\Utils\QueryHelper;
 use \DateTime;
 use \DateTimeZone;
+
+
 /**
  * Grade controller.
  *
@@ -27,7 +31,36 @@ class GradeController extends Controller
     {
         $entity = 'Grade';
         $em = $this->getDoctrine()->getManager();
-        $grades = $em->getRepository('AppBundle:Grade')->findAll();
+
+
+        //CODE TO CHECK TO SEE IF CAMPAIGN EXISTS
+        $campaign = $em->getRepository('AppBundle:Campaign')->findOneByUrl($campaignUrl);
+        $accessFail = false;
+        //Does Campaign Exist? if not, fail
+        if(is_null($campaign)){
+          $this->get('session')->getFlashBag()->add('warning', 'We are sorry, we could not find this campaign.');
+          return $this->redirectToRoute('homepage', array('action'=>'list_campaigns'));
+        //If it does exist, is it "offline"? if not, fail
+        }elseif(!$campaign->getOnlineFlag()){
+          $securityContext = $this->container->get('security.authorization_checker');
+          //If it is offline, is a user logged in? If not, fail
+          if ($securityContext->isGranted('ROLE_USER')) {
+            $campaignHelper = new CampaignHelper($em, $logger);
+            //Does that user have access to the campaign? If not, fail
+            if(!$campaignHelper->campaignPermissionsCheck($this->get('security.token_storage')->getToken()->getUser(), $campaign)){
+              $this->get('session')->getFlashBag()->add('warning', 'We are sorry, we could not find this campaign.');
+              return $this->redirectToRoute('homepage', array('action'=>'list_campaigns'));
+            }
+          }else{
+            $this->get('session')->getFlashBag()->add('warning', 'We are sorry, we could not find this campaign.');
+            return $this->redirectToRoute('homepage', array('action'=>'list_campaigns'));
+          }
+        }elseif($campaign->getStartDate() > new DateTime("now")){
+          return $this->redirectToRoute('campaign_splash', array('campaignUrl'=>$campaign->getUrl(), 'campaign'=>$campaign));
+        }
+
+        $grades = $em->getRepository('AppBundle:Grade')->findBy(array("campaign"=>$campaign));
+
 
         return $this->render('campaign/grade.index.html.twig', array(
             'grades' => $grades,
