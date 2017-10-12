f1cba0d55243014e5379c9aa6c9f3148df91d317
David Larrimore <davidlarrimore@gmail.com>
2017-10-11 23:46:36 -0400

    Password Recovery Functionality

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -119,4 +119,189 @@ class SecurityController extends Controller
 
       return $this->redirectToRoute('campaign_index', array('campaignUrl' => $campaign->getUrl()));
   }
+
+
+
+  /**
+   * Allows user to reset password
+   *
+   * @Route("/password_reset_form", name="password_reset_form")
+   *
+   */
+    public function passwordResetFormAction(Request $request, $campaignUrl)
+    {
+
+        $logger = $this->get('logger');
+        $logger->debug("Entering RegistrationController->passwordResetAction");
+        $em = $this->getDoctrine()->getManager();
+
+        $campaign = $em->getRepository('AppBundle:Campaign')->findOneByUrl($campaignUrl);
+
+        if ($request->isMethod('POST')) {
+            $fail = false;
+            $params = $request->request->all();
+
+            if(empty($params['user']['email'])){
+              $this->addFlash('warning','You must entter an email');
+              $fail = true;
+            }
+
+            if(!$fail){
+              $user = $em->getRepository('AppBundle:User')->findOneByEmail($params['user']['email']);
+              if(empty($user)){
+                $this->addFlash('warning','We are sorry, we could not find this account');
+                $fail = true;
+              }else{
+                $user->setPasswordResetCode(strtoupper(md5(uniqid(rand(), true))));
+                $user->setPasswordResetCodeTimestamp(new \DateTime());
+                $em->persist($user);
+                $em->flush();
+              }
+            }
+
+            if(!$fail){
+
+              //Send Email
+              $message = (new \Swift_Message("FR Manager Password Reset Request"))
+                ->setFrom('funrun@lrespto.org') //TODO: Change this to parameter for support email
+                ->setTo($user->getEmail())
+                ->setContentType("text/html")
+                ->setBody(
+                    $this->renderView('email/password_resetting.email.twig', array('campaign' => $campaign, 'user' => $user))
+                );
+
+              $this->get('mailer')->send($message);
+
+              $this->get('session')->getFlashBag()->add('info', 'Please check your inbox for more information on changing your password');
+              return $this->redirectToRoute('login', array('campaignUrl' => $campaign->getUrl()));
+            }
+
+        }
+
+        return $this->render('security/password.reset.form.html.twig', array(
+          'campaign' => $campaign,
+        ));
+
+
+    }
+
+
+    /**
+     * Allows user to reset password
+     *
+     * @Route("/password_reset", name="password_reset")
+     *
+     */
+      public function passwordResetAction(Request $request, $campaignUrl)
+      {
+
+          $logger = $this->get('logger');
+          $logger->debug("Entering RegistrationController->passwordResetAction");
+          $em = $this->getDoctrine()->getManager();
+
+          $campaign = $em->getRepository('AppBundle:Campaign')->findOneByUrl($campaignUrl);
+
+          if(null !== $request->query->get('password_reset_token') && null !== $request->query->get('email')){
+              $fail = false;
+              $passwordResetToken = $request->query->get('password_reset_token');
+              $userEmail = $request->query->get('email');
+              $user = $em->getRepository('AppBundle:User')->findOneByEmail(urldecode($userEmail));
+
+              if(empty($user)){
+                $this->addFlash('warning','We are sorry, we could not find this account');
+                $fail = true;
+              }
+
+              if(!$fail && $user->getPasswordResetCode() !== $passwordResetToken){
+                $this->addFlash('warning','We are sorry, This token could not be validated');
+                $fail = true;
+              }
+
+              if(!$fail){
+                $dateNow = (new \DateTime());
+                $userPasswordResetCodeTimestamp = $user->getPasswordResetCodeTimestamp()->modify('+30 minutes');
+                if($userPasswordResetCodeTimestamp < $dateNow){
+                  $logger->debug("Password Reset Code is expired");
+                  $this->addFlash('warning','This confirmation code is expired');
+                  $fail = true;
+                }
+              }
+
+              if (!$fail && $request->isMethod('POST')) {
+                  $fail = false;
+                  $params = $request->request->all();
+
+                  if(!$fail && (empty($params['user']['password']['first']) || empty($params['user']['password']['second']))){
+                    $this->addFlash('warning','Please confirm your password');
+                    $fail = true;
+                  }
+
+                  if(!$fail && ($params['user']['password']['first'] !== $params['user']['password']['second'])){
+                    $this->addFlash('warning','Passwords do not match');
+                    $fail = true;
+                  }
+
+
+                  if(!$fail){
+                    $user->setPasswordResetCode = null;
+                    $encoder = $this->container->get('security.password_encoder');
+                    $encoded = $encoder->encodePassword($user, $params['user']['password']['first']);
+                    $user->setPassword($encoded);
+
+                    $em->persist($user);
+                    $em->flush();
+
+
+                    //Send Email
+                    $message = (new \Swift_Message("FR Manager Password Reset Notification"))
+                      ->setFrom('funrun@lrespto.org') //TODO: Change this to parameter for support email
+                      ->setTo($user->getEmail())
+                      ->setContentType("text/html")
+                      ->setBody(
+                          $this->renderView('email/password_reset.email.twig', array('campaign' => $campaign, 'user' => $user))
+                      );
+
+                    $this->get('mailer')->send($message);
+
+
+
+                    $logger->info("Password has been reset for user:".$user->getEmail());
+                    $this->addFlash('success','Password has been reset!');
+                    return $this->redirectToRoute('login', array('campaignUrl' => $campaign->getUrl()));
+
+                  }
+              }
+
+          }else{
+            $logger->debug("There was an issue with this URL, please try again");
+            return $this->redirectToRoute('password_reset_form', array('campaignUrl' => $campaign->getUrl()));
+
+          }
+
+          return $this->render('security/password.reset.html.twig', array(
+            'campaign' => $campaign,
+            'user' => $user
+          ));
+
+
+      }
+
+
+
+
+
+
+
+
+  private function generateRandomString($length = 10) {
+      $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      $charactersLength = strlen($characters);
+      $randomString = '';
+      for ($i = 0; $i < $length; $i++) {
+          $randomString .= $characters[rand(0, $charactersLength - 1)];
+      }
+      return $randomString;
+  }
+
+
 }
