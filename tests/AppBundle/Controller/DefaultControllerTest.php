d6a10e7ac95671b6beabec1e0f475c4f1a97f06c
David Larrimore <davidlarrimore@gmail.com>
2017-10-15 08:28:39 -0400

    Testing back-on-track!!!

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -5,11 +5,13 @@ use AppBundle\Utils\QueryHelper;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+
+//This test case will test the HTTPS redirect
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = static::createClient(array(),array('HTTPS' => true));
 
         $crawler = $client->request('GET', '/');
 
