d7aa81b8a4e731d3f7751b81796d3e7b8eece6ab
David Larrimore <davidlarrimore@gmail.com>
2018-09-28 09:40:40 -0400

    broke out team view page front end in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,9 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('homepage', './assets/js/homepage.js') 
-    .addEntry('donation', './assets/js/donation.js')      
+    .addEntry('donation', './assets/js/donation.js')   
+    .addEntry('team-edit', './assets/js/team-edit.js')  
+
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
