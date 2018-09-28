8086e830fb747071698ca627061f86f162595670
David Larrimore <davidlarrimore@gmail.com>
2018-09-28 11:03:39 -0400

    Updated images on front page

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ Encore
     .addEntry('app', './assets/js/app.js')
     .addEntry('homepage', './assets/js/homepage.js') 
     .addEntry('donation-index', './assets/js/donation-index.js')   
+    .addEntry('campaign-index', './assets/js/campaign-index.js')   
     .addEntry('team-edit', './assets/js/team-edit.js')  
 
     //.addEntry('page1', './assets/js/page1.js')
